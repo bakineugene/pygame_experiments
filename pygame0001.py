e61cd2b4583847df4311d6b1a8b272a37e161ab8
bakineugene <zxlmm@ya.ru>
2020-06-11 13:43:08 +0500

    Draws grid

--- pygame0001.py
+++ pygame0001.py
@@ -1,6 +1,6 @@
 import pygame
 
-RESOLUTION_DEFAULT = (640, 480)
+RESOLUTION_DEFAULT = (1024, 768)
 
 POSITION_ZERO = (0, 0)
 
@@ -18,15 +18,14 @@ def handle_events_quit(event, game):
             game.is_running = False
 
 def draw_grid_element(surface, colour, size, point):
-    point_plus = point + size
-    pygame.draw.lines(surface, colour, True, ((point, point), (point, point_plus), (point_plus, point_plus), (point_plus, point)))
+    point_plus_x = point[0] + size
+    point_plus_y = point[1] + size
+    pygame.draw.lines(surface, colour, True, (point, (point[0], point_plus_y), (point_plus_x, point_plus_y), (point_plus_x, point[1])))
 
-def draw_grid(surface, colour=COLOR_BLACK, size=20):
-    draw_grid_element(surface, colour, size, 0)
-    draw_grid_element(surface, colour, size, size)
-    draw_grid_element(surface, colour, size, size * 2)
-    draw_grid_element(surface, colour, size, size * 3)
-    draw_grid_element(surface, colour, size, size * 4)
+def draw_grid(surface, colour=COLOR_BLACK, size=60, size_x=100, size_y=100):
+    for x in range(size_x):
+        for y in range(size_y):
+            draw_grid_element(surface, colour, size, (x * size, y * size))
 
 class Game(object):
 
@@ -51,7 +50,7 @@ class Game(object):
     def draw(self):
         self.timer = self.timer + 1
         self.screen.blit(self.background, POSITION_ZERO)
-        self.screen.blit(self.grid, (POSITION_ZERO[0] + self.timer, POSITION_ZERO[1]))
+        self.screen.blit(self.grid, POSITION_ZERO)
         pygame.display.flip()
 
     def start(self):
